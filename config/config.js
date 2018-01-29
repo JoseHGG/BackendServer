f3d146f1f54eaf18eb0cec73bd1f2a4e3c23bb02
José Guerrero <jh_guerrero_g@hotmail.com>
2018-01-28 23:59:06 -0500

    Backend Server terminado

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID =
+    '146853924442-866ogi0iodj1vgomn5iev810ev55ersj.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '8UFEGhubZVuxqwEOx9ug6w_G';
